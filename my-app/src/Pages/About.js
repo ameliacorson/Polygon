d1359b7dac8174209bf86203d9ab7cb91c7347e8
ameliacorson <92054473+ameliacorson@users.noreply.github.com>
2022-02-16 17:52:51 -0600

    initialize map api

--- my-app/src/Pages/About.js
+++ my-app/src/Pages/About.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Map from "../components/Map";
+import { Wrapper } from "@googlemaps/react-wrapper";
 
 export default function About() {
   return (
     <section class="about container">
-      <h2>Our Story</h2>
+      <h2>
+        About <span>Us</span>
+      </h2>
+      <div className="about-details">
+        <h4>Map</h4>
+        
+        <p></p>
+      </div>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure impedit
         cumque, architecto nihil rem omnis veniam, quidem officia at accusamus
---
+++ my-app/src/components/Map.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+
+
+export default function Map() {({
+        center,
+        zoom,
+      }: {
+        center: google.maps.LatLngLiteral;
+        zoom: number;
+      }) {
+        const ref = useRef();
+      
+        useEffect(() => {
+          new window.google.maps.Map(ref.current, {
+            center,
+            zoom,
+          });
+        });
+      
+        return <div ref={ref} id="map" />;
+      }
+    
+}
