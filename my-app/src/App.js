a7057c371b3b449311e1d24cf39e483d9cd0d618
ameliacorson <92054473+ameliacorson@users.noreply.github.com>
2022-02-17 11:35:53 -0600

    Removed "switch"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Switch, Route } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Nav from "./components/Nav";
@@ -10,11 +10,6 @@ export default function App() {
     <div>
       <Nav />
       <Header />
-      <Switch>
-        <Route path="/about">
-          <About />
-        </Route>
-      </Switch>
 
     </div>
   );
