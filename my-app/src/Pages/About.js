d677d014c06b84bed2adbeacb7ea077045e56607
ameliacorson <92054473+ameliacorson@users.noreply.github.com>
2022-03-14 18:44:55 -0500

    added unstyled hours

--- my-app/src/Pages/About.js
+++ my-app/src/Pages/About.js
@@ -4,7 +4,7 @@ import Map from "../components/Map";
 export default function About() {
   return (
     <div>
-       <Map />
+      <Map />
       <section className="about container">
         <h2>
           About <span>Us</span>
@@ -17,6 +17,16 @@ export default function About() {
             maxime non dicta sunt placeat.
           </p>
         </div>
+        <h3>Hours</h3>
+        <ul>
+          <li>Monday: 11:30am - 9:30pm</li>
+          <li>Tuesday: 11:30am - 9:30pm</li>
+          <li>Wednesday: 11:30am - 9:30pm</li>
+          <li>Thursday: 11:30am - 9:30pm</li>
+          <li>Friday: 11:30am - 9:30pm</li>
+          <li>Saturday: 11:30am - 9:30pm</li>
+          <li>Sunday: 11:30am - 9:30pm</li>
+        </ul>
       </section>
     </div>
   );
