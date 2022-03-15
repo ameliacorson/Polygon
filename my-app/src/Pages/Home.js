6d07d759c54985aec8c5709687534a98879d9b47
ameliacorson <92054473+ameliacorson@users.noreply.github.com>
2022-03-14 19:47:29 -0500

    fixed unused link error

--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { Carousel } from "react-bootstrap";
 import sushiImg from "../img/sushi platter.jpeg";
 import riceImg from "../img/rice.jpeg"
