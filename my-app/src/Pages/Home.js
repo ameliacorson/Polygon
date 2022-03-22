ecb83d03a79e40d7b745fd75bc9cdd7dc55ecf34
ameliacorson <92054473+ameliacorson@users.noreply.github.com>
2022-03-22 12:35:16 -0500

    use mapfunction for carousel

--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -1,51 +1,10 @@
 import React from "react";
-import { Carousel } from "react-bootstrap";
-import sushiImg from "../img/sushi platter.jpeg";
-import riceImg from "../img/rice.jpeg"
-import sushiRollImg from "../img/sushi-roll-img.jpeg"
-import friedRiceImg from "../img/fried-rice-img.jpeg"
-import sushiPlate from "../img/sushiPlate.jpeg"
+import ImgCarousel from "../components/ImgCarousel";
 
 export default function Home() {
   return (
     <div className="home">
-      <Carousel fade>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={sushiImg}
-            alt="First slide"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={riceImg}
-            alt=" slide"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={sushiRollImg}
-            alt="First slide"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={friedRiceImg}
-            alt="First slide"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={sushiPlate}
-            alt="First slide"
-          />
-        </Carousel.Item>
-      </Carousel>
+    <ImgCarousel />
       <div className="container">
         <h2> Chicago's Best Thai</h2>
 
