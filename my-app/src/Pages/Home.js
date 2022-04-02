cefae7e06bc8a29919cc9845eee4abbcae090195
ameliacorson <92054473+ameliacorson@users.noreply.github.com>
2022-04-02 13:22:51 -0500

    react-parallax and darken background image

--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ImgCarousel from "../components/ImgCarousel";
+import { Parallax, Background } from "react-parallax";
 
 import bwrestaurant from "../img/bw-restaurant.jpeg";
+import heroThai from "../img/hero-thai.jpeg";
 
 export default function Home() {
+
+  const styles = {backgroundBlendMode: 'darken'}
+
   return (
     <div className="home">
-      <div className="wrapper">
-        <div className="parallax-img">
-        <h2> Chicago's Best Thai</h2>
-        </div>
-      </div>
+      <Parallax blur={{ min: -15, max: 0 }} strength={300} bgImage={heroThai}>
+        <div className="hero">
+          <h2> Chicago's Best Thai</h2>
+          </div>
+      </Parallax>
 
       <div className="container">
         <p>
