816f4548d9f4d507c9d03224cfe6ce73e8bb2beb
ameliacorson <92054473+ameliacorson@users.noreply.github.com>
2022-04-03 12:42:03 -0500

    adjust footer and order-online height

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <div className="footer">
       <Parallax strength={300} bgImage={footerBackground}>
-      <div className="footer-container container">
+      <div className="footer-container">
         <div className="footer-info">
           <a href="https://www.facebook.com/Polygon-Cafe-260970567260627/" ><AiFillFacebook color="white" fontSize="3em" /></a>
           <a href="https://www.twitter.com/" ><AiFillTwitterSquare color="white" fontSize="3em" /></a>
