96ab926e8a5e579f4acc60859906df56160b858a
ameliacorson <92054473+ameliacorson@users.noreply.github.com>
2022-04-03 11:33:48 -0500

    format about content on home page

--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -9,13 +9,13 @@ import Map from "../components/Map";
 export default function Home() {
   return (
     <div className="home">
-      <Parallax strength={300} bgImage={heroThai}>
-        <div className="hero">
-          <h2> Chicago's Best Thai</h2>
-        </div>
-      </Parallax>
-
-      <div className="container">
+      
+        <Parallax strength={300} bgImage={heroThai}>
+          <div className="hero">
+            <h2> Chicago's Best Thai</h2>
+          </div>
+        </Parallax>
+        <div className="container">
         <h2>Welcome to Polygon Cafe</h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -30,32 +30,36 @@ export default function Home() {
             <button className="CTA-btn">order now </button>
           </Link>
         </div>
-        <p>
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est
-          laborum.
-        </p>
-      </div>
-      <div className="map-container container">
-        <Map />
-      </div>
-      <section className="about container">
-        <div className="about-details">
+      
+
+        <section className="about ">
+          <div className="about-details">
           <h2>About Us</h2>
+
+          <p>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
+
+          <div className="map-container container">
+            <Map />
+          </div>
+
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
             impedit cumque, architecto nihil rem omnis veniam, quidem officia at
             accusamus mollitia voluptate fugiat distinctio, exercitationem
             maxime non dicta sunt placeat.
           </p>
-        </div>
-        <div className="about-details">
+
           <h2>Hours</h2>
           <h4>Open Daily</h4>
           <p className="hours">11:30am - 9:30pm</p>
         </div>
-      </section>
+        </section>
+        </div>
     </div>
   );
 }
