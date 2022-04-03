3bf6ce81c906ab11cfa27085087ef8b6afbf19be
ameliacorson <92054473+ameliacorson@users.noreply.github.com>
2022-04-03 11:43:08 -0500

    format about section details

--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -9,13 +9,12 @@ import Map from "../components/Map";
 export default function Home() {
   return (
     <div className="home">
-      
-        <Parallax strength={300} bgImage={heroThai}>
-          <div className="hero">
-            <h2> Chicago's Best Thai</h2>
-          </div>
-        </Parallax>
-        <div className="container">
+      <Parallax strength={300} bgImage={heroThai}>
+        <div className="hero">
+          <h2> Chicago's Best Thai</h2>
+        </div>
+      </Parallax>
+      <div className="container">
         <h2>Welcome to Polygon Cafe</h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -30,10 +29,8 @@ export default function Home() {
             <button className="CTA-btn">order now </button>
           </Link>
         </div>
-      
 
         <section className="about ">
-          <div className="about-details">
           <h2>About Us</h2>
 
           <p>
@@ -54,12 +51,25 @@ export default function Home() {
             maxime non dicta sunt placeat.
           </p>
 
-          <h2>Hours</h2>
-          <h4>Open Daily</h4>
-          <p className="hours">11:30am - 9:30pm</p>
-        </div>
+          <div className="about-details">
+            <div className="about-details-child">
+              <h2>Hours</h2>
+              <h4>Open Daily</h4>
+              <p className="hours">11:30am - 9:30pm</p>
+            </div>
+            <div className="about-details-child">
+              <h2>Location</h2>
+              <p>5204 N Clark St</p>
+              <p>Chicago, IL 60640</p>
+            </div>
+            <div className="about-details-child">
+              <h2>Contact Us</h2>
+              <p>(773) 555-1234</p>
+              <p>info@polygon.com</p>
+            </div>
+          </div>
         </section>
-        </div>
+      </div>
     </div>
   );
 }
--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -5,14 +5,7 @@ export default function Footer() {
     <div className="footer">
       <div className="footer-container container">
         <div className="footer-info">
-          <h4>Location</h4>
-          <p>5204 N Clark St</p>
-          <p>Chicago, IL 60640</p>
-        </div>
-        <div className="footer-info">
-          <h4>Contact Us</h4>
-          <p>(773) 555-1234</p>
-          <p>info@polygon.com</p>
+          
         </div>
       </div>
     </div>
